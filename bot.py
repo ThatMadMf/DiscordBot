d69c7ab1a9f1e9fadeffc226c5e9a9c8030c6ab2
Max Onoshko <maxdarksiders@gmail.com>
2020-07-04 13:13:43 +0300

    Prepare for deploy

---
+++ bot.py
@@ -0,0 +1,81 @@
+import os
+import random
+
+import discord
+import requests
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix='/')
+
+
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+@bot.command(name='join')
+async def join_voice(ctx):
+    connected = ctx.author.voice
+    if connected:
+        await connected.channel.connect()
+
+
+@bot.command(name='leave')
+async def leave_voice(ctx):
+    try:
+        for x in bot.voice_clients:
+            return await x.disconnect()
+    except Exception:
+        return ctx.send('WTF')
+
+
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+@bot.command(name='flip')
+async def flip(ctx):
+    coin = ['heads', 'tails']
+    response = f'You got {random.choice(coin)}, sir'
+    await ctx.send(response)
+
+
+@bot.command(name='dice')
+async def dice(ctx):
+    response = f'You got {random.randint(1, 6)} , sir'
+    await ctx.send(response)
+
+
+@bot.command(name='image')
+async def image(ctx):
+    try:
+        img_urls = ['https://imgur.com/random']
+        pp = requests.get(url=random.choice(img_urls))
+        response = pp.url
+        await ctx.send(response)
+    except Exception:
+        print(Exception)
+
+
+@bot.command(name='play', pass_context=True)
+async def play(ctx):
+    guild = ctx.guild
+    voice_client = discord.VoiceClient = discord.utils.get(bot.voice_clients, guild=guild)
+    audio_source = discord.FFmpegPCMAudio('shatt.mp3')
+    if not voice_client.is_playing():
+        voice_client.play(audio_source, after=None)
+
+bot.run(TOKEN)
